ad84beabeb91c4fce5b08fe422ce2af45221e56e
matthewslape <75864370+matthewslape@users.noreply.github.com>
2022-04-01 09:19:03 -0700

    setting up rows and columns for grid

--- src/views/Illustration.vue
+++ src/views/Illustration.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="Illustration">
     <h1 class="title">IllUSTRATION</h1>
-    <img
-      src="https://lh3.googleusercontent.com/Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g"
-    />
-    <!--<div class="column">
-      <div class="photoWrapper" v-for="item in column1" :key="item">
-        <img
-          :src="require(`https://lh3.googleusercontent.com/${item.key}=w2400`)"
-          alt="Project"
-        />
-        <h3 class="photoTitle">{{ item.title }}</h3>
+    <div class="row">
+      <div class="column">
+        <div class="photoWrapper" v-for="item in column1" :key="item">
+          <img
+            :src="`https://lh3.googleusercontent.com/${item.key}`"
+            alt="Project"
+          />
+          <h3 class="photoTitle">{{ item.title }}</h3>
+        </div>
       </div>
-    </div>-->
+      <div class="column">
+        <div class="photoWrapper" v-for="item in column2" :key="item">
+          <img
+            :src="`https://lh3.googleusercontent.com/${item.key}`"
+            alt="Project"
+          />
+          <h3 class="photoTitle">{{ item.title }}</h3>
+        </div>
+      </div>
+      <div class="column">
+        <div class="photoWrapper" v-for="item in column3" :key="item">
+          <img
+            :src="`https://lh3.googleusercontent.com/${item.key}`"
+            alt="Project"
+          />
+          <h3 class="photoTitle">{{ item.title }}</h3>
+        </div>
+      </div>
+      <div class="column">
+        <div class="photoWrapper" v-for="item in column4" :key="item">
+          <img
+            :src="`https://lh3.googleusercontent.com/${item.key}`"
+            alt="Project"
+          />
+          <h3 class="photoTitle">{{ item.title }}</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,26 +49,122 @@ export default {
       column1: [
         {
           key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
-          title: "JS Trucking and Excavation",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
         },
       ],
       column2: [
-        { key: "js", title: "JS Trucking and Excavation" },
-        { key: "ecp", title: "Energy Conservation Program" },
-        { key: "sphoto", title: "Samanth Rhea Photography" },
-        { key: "keyconnect", title: "KEYTHECONNECT" },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
       ],
       column3: [
-        { key: "js", title: "JS Trucking and Excavation" },
-        { key: "ecp", title: "Energy Conservation Program" },
-        { key: "sphoto", title: "Samanth Rhea Photography" },
-        { key: "keyconnect", title: "KEYTHECONNECT" },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
       ],
       column4: [
-        { key: "js", title: "JS Trucking and Excavation" },
-        { key: "ecp", title: "Energy Conservation Program" },
-        { key: "sphoto", title: "Samanth Rhea Photography" },
-        { key: "keyconnect", title: "KEYTHECONNECT" },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
+        {
+          key: "Y4bf2CtcgG8cvB9CfZ4PEkAWUkE9jl6pxo3-y2LA8dkE5FOJz1SlsXkJ69I_uwKpCkrLBvn0zw8PwBkRL-kycFwTvWKKiOvmF3PCrplZRDsx55kRQ1MRlZv08sYQjh2OQV09v3x95g",
+          title: "West Plains Tools",
+        },
       ],
     };
   },
@@ -50,13 +172,51 @@ export default {
 </script>
 
 <style scoped>
+/*title*/
+h1 {
+  margin-top: 7vw;
+  background: linear-gradient(
+    138.27deg,
+    #000000 0%,
+    #001d68 34.98%,
+    #1c6b6b 90.98%
+  );
+  background-clip: text;
+  color: transparent;
+}
+
+/* For individual photo */
 .photoWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.photoTitle {
+  font-size: 1.5rem;
+  position: absolute;
+  color: white;
+  z-index: 4;
+  opacity: 0;
+  transition-duration: 0.5s;
+}
+
+/* Hover */
+.column img:hover {
+  filter: blur(2px) grayscale(1);
+}
+
+.column img:hover ~ .photoTitle {
+  opacity: 1;
+}
+
+/* Row of columns that sits next to each other */
+.row {
   display: flex;
   flex-wrap: wrap;
   padding: 0 4px;
 }
 
-/* Create four equal columns that sits next to each other */
 .column {
   flex: 25%;
   max-width: 25%;
@@ -67,9 +227,10 @@ export default {
   margin-top: 8px;
   vertical-align: middle;
   width: 100%;
+  transition-duration: 0.5s;
 }
 
-/* Responsive layout - makes a two column-layout instead of four columns */
+/* Responsive layout */
 @media screen and (max-width: 800px) {
   .column {
     flex: 50%;
